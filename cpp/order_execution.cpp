670a4976aa6d7b786bfc5a72fb5672d0fd04c3b8
SuperLuckyWin <yunbarbalichcxc31@gmail.com>
2025-02-13 15:54:33 +0800

    Delete cpp directory

--- cpp/order_execution.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <string>
-
-extern "C" {
-    void execute_order(const char* order) {
-        std::string order_str(order);
-        std::cout << "Executing order: " << order_str << std::endl;
-    }
-} 
